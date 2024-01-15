8ac00390bec304a75ba5a92c6a95ae214e6a6383
LNR14 <lnrabc@gmail.com>
2024-01-15 11:56:00 -0500

    Completed profile

--- insta_clone/src/components/FeedPosts/PostFooter.tsx
+++ insta_clone/src/components/FeedPosts/PostFooter.tsx
@@ -14,7 +14,7 @@ import {
   UnlikeLogo,
 } from "../../assets/constants";
 
-const PostFooter = ({ username }) => {
+const PostFooter = ({ username, isProfilePage }) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(1000);
 
@@ -28,7 +28,7 @@ const PostFooter = ({ username }) => {
     }
   };
   return (
-    <Box mb={10}>
+    <Box mb={10} marginTop={"auto"}>
       <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
         <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
           {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
@@ -40,16 +40,19 @@ const PostFooter = ({ username }) => {
       <Text fontWeight={600} fontSize={"sm"}>
         {likes} likes
       </Text>
-
-      <Text fontSize={"sm"} fontWeight={700}>
-        {username}
-        <Text fontSize={"sm"} fontWeight={400}>
-          Feeling good
-        </Text>
-      </Text>
-      <Text fontSize={"sm"} fontWeight={200}>
-        View all 1,000 comments
-      </Text>
+      {!isProfilePage && (
+        <>
+          <Text fontSize={"sm"} fontWeight={700}>
+            {username}
+            <Text fontSize={"sm"} fontWeight={400}>
+              Feeling good
+            </Text>
+          </Text>
+          <Text fontSize={"sm"} fontWeight={200}>
+            View all 1,000 comments
+          </Text>
+        </>
+      )}
       <Flex
         alignItems={"center"}
         gap={2}
--- insta_clone/src/components/Profile/ProfileTabs.tsx
+++ insta_clone/src/components/Profile/ProfileTabs.tsx
@@ -1,5 +1,5 @@
-import { Box, Flex } from "@chakra-ui/react";
-import { BsGrid3X3 } from "react-icons/bs";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { BsBookmark, BsGrid3X3, BsSuitHeart } from "react-icons/bs";
 
 const ProfileTabs = () => {
   return (
@@ -11,13 +11,36 @@ const ProfileTabs = () => {
         textTransform={"uppercase"}
         fontWeight={"bold"}
       >
-        <Flex borderTop={"1px solid white"}>
+        <Flex
+          borderTop={"1px solid white"}
+          alignItems={"center"}
+          p="3"
+          gap={1}
+          cursor={"pointer"}
+        >
           <Box fontSize={20}>
             <BsGrid3X3 />
           </Box>
+          <Text fontSize={12} display={{ base: "none", sm: "block" }}>
+            Posts
+          </Text>
         </Flex>
-        <Flex></Flex>
-        <Flex></Flex>
+        <Flex alignItems={"center"} p="3" gap={1} cursor={"pointer"}>
+          <Box fontSize={20}>
+            <BsBookmark />
+          </Box>
+          <Text fontSize={12} display={{ base: "none", sm: "block" }}>
+            Saved
+          </Text>
+        </Flex>
+        <Flex alignItems={"center"} p="3" gap={1} cursor={"pointer"}>
+          <Box fontSize={20}>
+            <BsSuitHeart fontWeight={"bold"} />
+          </Box>
+          <Text fontSize={12} display={{ base: "none", sm: "block" }}>
+            Likes
+          </Text>
+        </Flex>
       </Flex>
     </>
   );
